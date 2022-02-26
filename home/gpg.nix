a60b63e8e68bcb3fa4857c500869344d16daba8c
Jonny Powell <jonnyowenpowell@gmail.com>
2022-02-26 18:53:31 +0000

    Use correct gpg-agent program

--- home/gpg.nix
+++ home/gpg.nix
@@ -1,12 +1,12 @@
 { config, pkgs, ... }:
 
 {
-  services.gpg-agent = {
-    enable = true;
-    defaultCacheTtl = 1800;
-    enableSshSupport = true;
-    extraConfig = ''
-      pinentry-program ${pkgs.pinentry_mac}/Applications/pinentry-mac.app/Contents/MacOS/pinentry-mac
-    '';
+  programs.gnupg.agent = {
+      enable = true;
+      enableSSHSupport = true;
+      defaultCacheTtl = 1800;
+      extraConfig = ''
+        pinentry-program ${pkgs.pinentry_mac}/Applications/pinentry-mac.app/Contents/MacOS/pinentry-mac
+      '';
   };
 }
