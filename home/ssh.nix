ae52c7de170e1c28f2279ebbe28640b9376d6b5a
Jonny Powell <jonnyowenpowell@gmail.com>
2022-02-27 21:04:42 +0000

    Fixed ssh!

--- home/ssh.nix
+++ home/ssh.nix
@@ -3,17 +3,8 @@
 {
   home.file.".ssh/config".text = ''
     Host *
-      IdentitiesOnly yes
       IdentityAgent /Users/jonny/Library/Containers/com.maxgoedjen.Secretive.SecretAgent/Data/socket.ssh
-      AddKeysToAgent yes
-      ForwardAgent yes
-      Compression no
-      ServerAliveInterval 60
-      ServerAliveCountMax 3
       HashKnownHosts yes
       UserKnownHostsFile /Users/jonny/.ssh/known_hosts
-      ControlMaster no
-      ControlPath ~/.ssh/master-%r@%n:%p
-      ControlPersist no
   '';
 }
