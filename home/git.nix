dae853512ddec3f7e0c66a124f87e4c5ec7cc5ed
Jonny Powell <jonnyowenpowell@gmail.com>
2022-02-27 20:07:05 +0000

    Updated git and ssh config

--- home/git.nix
+++ home/git.nix
@@ -9,8 +9,9 @@
   programs.git.extraConfig = {
     advice.addignoredfile = false;
     commit.gpgsign = true;
-    core.editor = "${pkgs.neovim-remote}/bin/nvr --remote-wait-silent -cc split";
     core.autocrlf = "input";
+    core.editor = "${pkgs.neovim-remote}/bin/nvr --remote-wait-silent -cc split";
+    core.sshCommand = "/usr/bin/ssh";
     credential.helper = "osxkeychain";
     diff.colorMoved = "default";
     init.defaultbranch = "main";
--- home/ssh.nix
+++ home/ssh.nix
@@ -1,22 +1,19 @@
 { config, ... }:
 
 {
-  programs.ssh.enable = true;
-
-  programs.ssh.forwardAgent = true;
-  programs.ssh.serverAliveInterval = 60;
-
-  programs.ssh.hashKnownHosts = true;
-  programs.ssh.userKnownHostsFile = "${config.home.homeDirectory}/.ssh/known_hosts";
-
-  programs.ssh.matchBlocks = {
-    id_ed25519 = {
-      host = "*";
-      identityFile = "${config.home.homeDirectory}/.ssh/id_ed25519";
-      identitiesOnly = true;
-      extraOptions = {
-        AddKeysToAgent = "yes";
-      };
-    };
-  };
+  home.file.".ssh/config".text = ''
+    Host *
+      IdentitiesOnly yes
+      IdentityFile /Users/jonny/.ssh/id_ed25519
+      AddKeysToAgent yes
+      ForwardAgent yes
+      Compression no
+      ServerAliveInterval 60
+      ServerAliveCountMax 3
+      HashKnownHosts yes
+      UserKnownHostsFile /Users/jonny/.ssh/known_hosts
+      ControlMaster no
+      ControlPath ~/.ssh/master-%r@%n:%p
+      ControlPersist no
+  '';
 }
