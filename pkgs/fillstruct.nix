f78976b69797902564bb487515214a49f7ce05d4
Jonny Powell <jonnyowenpowell@gmail.com>
2022-03-05 23:45:45 +0000

    Added fillstruct sha256

--- pkgs/fillstruct.nix
+++ pkgs/fillstruct.nix
@@ -8,11 +8,11 @@ buildGoModule rec {
     owner = "davidrjenni";
     repo = "reftools";
     rev = "40322ffdc2e46fd7920d1f8250051bbd2f3bd34d";
-    sha256 = lib.fakeSha256;
+    sha256 = "sha256-fHWtUoVK3G0Kn69O6/D0blM6Q/u4LuLinT6sxF18nFo=";
   };
 
   modRoot = "cmd/fillstruct";
-  vendorSha256 = lib.fakeSha256;
+  vendorSha256 = "sha256-pQpattmS9VmO3ZIQUFn66az8GSmB4IvYhTTCFn6SUmo=";
 
   doCheck = false;
 }
