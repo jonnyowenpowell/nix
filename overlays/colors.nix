a716f84fe1dae91dc0229c6ef02abc2066967c91
Jonny Powell <jonnyowenpowell@gmail.com>
2022-03-06 22:31:47 +0000

    Added rgb to colors.nix

--- overlays/colors.nix
+++ overlays/colors.nix
@@ -1,45 +1,45 @@
 # Colors from https://github.com/rebelot/kanagawa.nvim
 # Used in Kitty terminal config: `./kitty-configs.nix`
 
-# kanagawa       hex     base16 usage
-# -------------- ------- ------ ---------------------------------------------------------------------------------
-# fujiWhite      #dcd7ba   05   Default foreground
-# oldWhite       #c8c093   04   Dark foreground (statuslines)
-# sumiInk0       #16161d        Dark background (statuslines and floating windows)
-# sumiInk1       #1f1f28   00   Default background
-# sumiInk2       #2a2a37   01   Lighter background (colorcolumn, folds)
-# sumiInk3       #363646   07   Lighter background (cursorline)
-# sumiInk4       #54546d        Darker foreground (line numbers, fold column, non-text characters), float borders
-# waveBlue1      #223249   02   Popup background, visual selection background
-# waveBlue2      #2d4f67        Popup selection background, search background
-# winterGreen    #2b3328        Diff Add (background)
-# winterYellow   #49443c        Diff Change (background)
-# winterRed      #43242b        Diff Deleted (background)
-# winterBlue     #252535        Diff Line (background)
-# autumnGreen    #76946a        Git Add
-# autumnRed      #c34043   08   Git Delete
-# autumnYellow   #dca561   0A   Git Change
-# samuraiRed     #e82424        Diagnostic Error
-# roninYellow    #ff9e3b        Diagnostic Warning
-# waveAqua1      #6a9589        Diagnostic Info
-# dragonBlue     #658594        Diagnostic Hint
-# fujiGray       #727169   03   Comments
-# springViolet1  #938aa9   06   Light foreground
-# oniViolet      #957fb8   0E   Statements and Keywords
-# crystalBlue    #7e9cd8   0D   Functions and Titles
-# springViolet2  #9cabca        Brackets and punctuation
-# springBlue     #7fb4ca   0C   Specials and builtin functions
-# lightBlue      #a3d4d5        Not used
-# waveAqua2      #7aa89f        Types
-# springGreen    #98bb6c   0B   Strings
-# boatYellow1    #938056        Not used
-# boatYellow2    #c0a36e        Operators, RegEx
-# carpYellow     #e6c384        Identifiers
-# sakuraPink     #d27e99   0F   Numbers
-# waveRed        #e46876        Standout specials 1 (builtin variables)
-# peachRed       #ff5d62        Standout specials 2 (exception handling, return)
-# surimiOrange   #ffa066   09   Constants, imports, booleans
-# katanaGray     #717c7c        Deprecated
+# kanagawa       hex      rgb                base16 usage
+# -------------- -------  ------------------ ------ ---------------------------------------------------------------------------------
+# fujiWhite      #dcd7ba  rgb(220, 215, 186)   05   Default foreground
+# oldWhite       #c8c093  rgb(200, 192, 147)   04   Dark foreground (statuslines)
+# sumiInk0       #16161d  rgb(22, 22, 29)           Dark background (statuslines and floating windows)
+# sumiInk1       #1f1f28  rgb(31, 31, 40)      00   Default background
+# sumiInk2       #2a2a37  rgb(42, 42, 55)      01   Lighter background (colorcolumn, folds)
+# sumiInk3       #363646  rgb(54, 54, 70)      07   Lighter background (cursorline)
+# sumiInk4       #54546d  rgb(84, 84, 109)          Darker foreground (line numbers, fold column, non-text characters), float borders
+# waveBlue1      #223249  rgb(34, 50, 73)      02   Popup background, visual selection background
+# waveBlue2      #2d4f67  rgb(45, 79, 103)          Popup selection background, search background
+# winterGreen    #2b3328  rgb(43, 51, 40)           Diff Add (background)
+# winterYellow   #49443c  rgb(73, 68, 60)           Diff Change (background)
+# winterRed      #43242b  rgb(67, 36, 43)           Diff Deleted (background)
+# winterBlue     #252535  rgb(37, 37, 53)           Diff Line (background)
+# autumnGreen    #76946a  rgb(118, 148, 106)        Git Add
+# autumnRed      #c34043  rgb(195, 64, 67)     08   Git Delete
+# autumnYellow   #dca561  rgb(220, 165, 97)    0A   Git Change
+# samuraiRed     #e82424  rgb(232, 36, 36)          Diagnostic Error
+# roninYellow    #ff9e3b  rgb(255, 158, 59)         Diagnostic Warning
+# waveAqua1      #6a9589  rgb(106, 149, 137)        Diagnostic Info
+# dragonBlue     #658594  rgb(101, 133, 148)        Diagnostic Hint
+# fujiGray       #727169  rgb(114, 113, 105)   03   Comments
+# springViolet1  #938aa9  rgb(147, 138, 169)   06   Light foreground
+# oniViolet      #957fb8  rgb(149, 127, 184)   0E   Statements and Keywords
+# crystalBlue    #7e9cd8  rgb(126, 156, 216)   0D   Functions and Titles
+# springViolet2  #9cabca  rgb(156, 171, 202)        Brackets and punctuation
+# springBlue     #7fb4ca  rgb(127, 180, 202)   0C   Specials and builtin functions
+# lightBlue      #a3d4d5  rgb(163, 212, 213)        Not used
+# waveAqua2      #7aa89f  rgb(122, 168, 159)        Types
+# springGreen    #98bb6c  rgb(152, 187, 108)   0B   Strings
+# boatYellow1    #938056  rgb(147, 128, 86)         Not used
+# boatYellow2    #c0a36e  rgb(192, 163, 110)        Operators, RegEx
+# carpYellow     #e6c384  rgb(230, 195, 132)        Identifiers
+# sakuraPink     #d27e99  rgb(210, 126, 153)   0F   Numbers
+# waveRed        #e46876  rgb(228, 104, 118)        Standout specials 1 (builtin variables)
+# peachRed       #ff5d62  rgb(255, 93, 98)          Standout specials 2 (exception handling, return)
+# surimiOrange   #ffa066  rgb(255, 160, 102)   09   Constants, imports, booleans
+# katanaGray     #717c7c  rgb(113, 124, 124)        Deprecated
 
 final: prev: {
   lib = prev.lib // {
