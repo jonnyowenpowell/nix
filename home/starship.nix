25c32e062556813af5bbb5abf22bea72782de6dd
Jonny Powell <jonnyowenpowell@gmail.com>
2022-03-06 15:29:28 +0000

    Changed starship config

--- home/starship.nix
+++ home/starship.nix
@@ -15,11 +15,17 @@ in {
     # Symbols config configured ./starship-symbols.nix.
 
     battery.display.threshold = 25; # display battery information if charge is <= 25%
+
+    character.success_symbol = "[➜](bold green) ";
+    character.error_symbol = "[✗](bold red) ";
+
     directory.fish_style_pwd_dir_length = 1; # turn on fish directory truncation
     directory.truncation_length = 2; # number of directories not to truncate
-    gcloud.disabled = true; # annoying to always have on
+
     hostname.style = "bold green"; # don't like the default
+
     memory_usage.disabled = true; # because it includes cached memory it's reported as full a lot
+
     username.style_user = "bold blue"; # don't like the default
   };
 }
