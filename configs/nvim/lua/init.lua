b1576b0ddf20aeb61227850d0df906d5047159eb
Jonny Powell <jonnyowenpowell@gmail.com>
2022-03-06 21:29:50 +0000

    Misc basic additions to init.lua

--- configs/nvim/lua/init.lua
+++ configs/nvim/lua/init.lua
@@ -1,3 +1,41 @@
+local o = vim.o
+local wo = vim.wo
+local g = vim.g
+local cmd = vim.cmd
+local env = vim.env
+
+-- Basic Vim Config --------------------------------------------------------------------------------
+
+o.scrolloff  = 10   -- start scrolling when cursor is within 5 lines of the ledge
+o.linebreak  = true -- soft wraps on words not individual chars
+o.mouse      = ''   -- no mouse!
+o.updatetime = 300
+o.autochdir  = true
+o.exrc       = true -- allow project specific config in .nvimrc or .exrc files
+
+-- Search and replace
+o.ignorecase = true      -- make searches with lower case characters case insensative
+o.smartcase  = true      -- search is case sensitive only if it contains uppercase chars
+o.inccommand = 'nosplit' -- show preview in buffer while doing find and replace
+
+-- Tab key behavior
+o.expandtab  = true      -- Convert tabs to spaces
+o.tabstop    = 2         -- Width of tab character
+o.shiftwidth = o.tabstop -- Width of auto-indents
+
+-- Set where splits open
+o.splitbelow = true -- open horizontal splits below instead of above which is the default
+o.splitright = true -- open vertical splits to the right instead of the left with is the default
+
+-- UI ----------------------------------------------------------------------------------------------
+o.termguicolors   = true
+o.showmode        = false -- don't show -- INSERT -- etc.
+wo.colorcolumn    = '100' -- show column boarder
+wo.number         = true  -- display numberline
+wo.relativenumber = true  -- relative line numbers
+wo.signcolumn     = 'yes' -- always have signcolumn open to avoid thing shifting around all the time
+o.fillchars       = 'stl: ,stlnc: ,vert:·,eob: ' -- No '~' on lines after end of file, other stuff
+
 vim.cmd 'packadd kanagawa.nvim'
 
 require'kanagawa'.setup {
