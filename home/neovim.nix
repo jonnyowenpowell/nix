62b71031a63879f252c621a4583e518f1e6ec5cc
Jonny Powell <jonnyowenpowell@gmail.com>
2022-03-09 15:22:30 +0000

    Fixed packages in wrong file

--- home/neovim.nix
+++ home/neovim.nix
@@ -15,6 +15,28 @@ let
       sha256 = "sha256-TStFrx0u/MwSDcimvuwRRiyajkVq+rF5HWYCRsL4PCI=";
     };
   };
+
+  # go.nvim
+  go-nvim = pkgs.vimUtils.buildVimPluginFrom2Nix {
+    pname = "go.nvim";
+    name = "go.nvim"; # https://github.com/ray-x/go.nvim
+    src = pkgs.fetchFromGitHub {
+      owner = "ray-x";
+      repo = "go.nvim";
+      rev = "0fe0a9ee3367f7bd1e9c9ab30d5b7d5e66b83fc6";
+      sha256 = "sha256-DyByErbti+bBr0IZMQLwpGgrRJsFUeFK7buJyFjj1NE=";
+    };
+  };
+  guihua-lua = pkgs.vimUtils.buildVimPluginFrom2Nix {
+    pname = "guihua.lua";
+    name = "guihua.lua"; # https://github.com/ray-x/guihua.lua
+    src = pkgs.fetchFromGitHub {
+      owner = "ray-x";
+      repo = "guihua.lua";
+      rev = "dc31428364809b4388c007c227e3d5d39f1e4395";
+      sha256 = "sha256-Qi6aeHFrYChWbNr4sEE+6KPtTZSlmJRx2msYZK4Y/8s=";
+    };
+  };
 in
 {
   # Neovim
