0853c949105ca9945d168cef6808e27b6aaa66de
Jonny Powell <jonnyowenpowell@gmail.com>
2022-03-09 15:21:35 +0000

    Moved neovim.extraPackges to home.packages

--- home/neovim.nix
+++ home/neovim.nix
@@ -15,32 +15,6 @@ let
       sha256 = "sha256-TStFrx0u/MwSDcimvuwRRiyajkVq+rF5HWYCRsL4PCI=";
     };
   };
-
-  # go.nvim
-  go-nvim = pkgs.vimUtils.buildVimPluginFrom2Nix {
-    pname = "go.nvim";
-    name = "go.nvim"; # https://github.com/ray-x/go.nvim
-    src = pkgs.fetchFromGitHub {
-      owner = "ray-x";
-      repo = "go.nvim";
-      rev = "0fe0a9ee3367f7bd1e9c9ab30d5b7d5e66b83fc6";
-      sha256 = "sha256-DyByErbti+bBr0IZMQLwpGgrRJsFUeFK7buJyFjj1NE=";
-    };
-  };
-  guihua-lua = pkgs.vimUtils.buildVimPluginFrom2Nix {
-    pname = "guihua.lua";
-    name = "guihua.lua"; # https://github.com/ray-x/guihua.lua
-    src = pkgs.fetchFromGitHub {
-      owner = "ray-x";
-      repo = "guihua.lua";
-      rev = "dc31428364809b4388c007c227e3d5d39f1e4395";
-      sha256 = "sha256-Qi6aeHFrYChWbNr4sEE+6KPtTZSlmJRx2msYZK4Y/8s=";
-    };
-  };
-  golines = pkgs.callPackage ../pkgs/golines.nix {};
-  fillstruct = pkgs.callPackage ../pkgs/fillstruct.nix {};
-  fillswitch = pkgs.callPackage ../pkgs/fillswitch.nix {};
-  fixplurals = pkgs.callPackage ../pkgs/fixplurals.nix {};
 in
 {
   # Neovim
@@ -63,28 +37,5 @@ in
     nvim-treesitter
     plenary-nvim
     telescope-nvim
-  ];
-
-  programs.neovim.extraPackages = with pkgs; [
-    # language servers
-    gopls
-
-    # go.nvim
-    gofumpt
-    gomodifytags
-    gotests
-    iferr
-    impl
-    delve
-    richgo
-    golines
-    gotools
-    fillstruct
-    fillswitch
-    fixplurals
-
-    # nvim-treesitter
-    gcc
-    tree-sitter
   ];
 }
--- home/packages.nix
+++ home/packages.nix
@@ -1,6 +1,31 @@
 { config, lib, pkgs, ... }:
-
-{
+let
+  # go.nvim
+  go-nvim = pkgs.vimUtils.buildVimPluginFrom2Nix {
+    pname = "go.nvim";
+    name = "go.nvim"; # https://github.com/ray-x/go.nvim
+    src = pkgs.fetchFromGitHub {
+      owner = "ray-x";
+      repo = "go.nvim";
+      rev = "0fe0a9ee3367f7bd1e9c9ab30d5b7d5e66b83fc6";
+      sha256 = "sha256-DyByErbti+bBr0IZMQLwpGgrRJsFUeFK7buJyFjj1NE=";
+    };
+  };
+  guihua-lua = pkgs.vimUtils.buildVimPluginFrom2Nix {
+    pname = "guihua.lua";
+    name = "guihua.lua"; # https://github.com/ray-x/guihua.lua
+    src = pkgs.fetchFromGitHub {
+      owner = "ray-x";
+      repo = "guihua.lua";
+      rev = "dc31428364809b4388c007c227e3d5d39f1e4395";
+      sha256 = "sha256-Qi6aeHFrYChWbNr4sEE+6KPtTZSlmJRx2msYZK4Y/8s=";
+    };
+  };
+  golines = pkgs.callPackage ../pkgs/golines.nix {};
+  fillstruct = pkgs.callPackage ../pkgs/fillstruct.nix {};
+  fillswitch = pkgs.callPackage ../pkgs/fillswitch.nix {};
+  fixplurals = pkgs.callPackage ../pkgs/fixplurals.nix {};
+in {
   # Bat, a substitute for cat.
   # https://github.com/sharkdp/bat
   # https://rycee.gitlab.io/home-manager/options.html#opt-programs.bat.enable
@@ -26,6 +51,29 @@
     dotnet-sdk
     nodejs
     nodePackages.yarn
+
+    # Neovim
+    ## Language servers
+    gopls
+    omnisharp-roslyn
+
+    ## go.nvim
+    gofumpt
+    gomodifytags
+    gotests
+    iferr
+    impl
+    delve
+    richgo
+    golines
+    gotools
+    fillstruct
+    fillswitch
+    fixplurals
+
+    ## nvim-treesitter
+    gcc
+    tree-sitter
 
     # Nix
     comma
