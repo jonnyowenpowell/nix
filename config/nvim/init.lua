563446e283d89bf3b160e560d174707cbff976b3
Jonny Powell <jonnyowenpowell@gmail.com>
2022-08-02 16:05:57 +0100

    Updated neovim config

---
+++ config/nvim/init.lua
@@ -0,0 +1,202 @@
+local config = {
+
+  updater = {
+    remote = "origin",
+    channel = "stable",
+    version = "v1.7.*",
+    pin_plugins = true,
+    skip_prompts = true,
+    show_changelog = false,
+  },
+
+  -- Set colorscheme
+  colorscheme = "default_theme",
+
+  -- set vim options here (vim.<first_key>.<second_key> =  value)
+  options = {
+    opt = {
+      relativenumber = true, -- sets vim.opt.relativenumber
+    },
+    g = {
+      mapleader = " ", -- sets vim.g.mapleader
+    },
+  },
+
+  -- Configure plugins
+  plugins = {
+    -- Add plugins, the packer syntax without the "use"
+    init = {
+      -- You can disable default plugins as follows:
+      -- ["goolord/alpha-nvim"] = { disable = true },
+
+      -- You can also add new plugins here as well:
+      -- { "andweeb/presence.nvim" },
+      -- {
+      --   "ray-x/lsp_signature.nvim",
+      --   event = "BufRead",
+      --   config = function()
+      --     require("lsp_signature").setup()
+      --   end,
+      -- },
+    },
+    -- All other entries override the setup() call for default plugins
+    ["null-ls"] = function(config)
+      local null_ls = require "null-ls"
+      -- Check supported formatters and linters
+      -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
+      -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
+      config.sources = {
+        -- Set a formatter
+        null_ls.builtins.formatting.rufo,
+        -- Set a linter
+        null_ls.builtins.diagnostics.rubocop,
+      }
+      -- set up null-ls's on_attach function
+      config.on_attach = function(client)
+        -- NOTE: You can remove this on attach function to disable format on save
+        if client.resolved_capabilities.document_formatting then
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            desc = "Auto format before save",
+            pattern = "<buffer>",
+            callback = vim.lsp.buf.formatting_sync,
+          })
+        end
+      end
+      return config -- return final config table
+    end,
+    treesitter = {
+      ensure_installed = { "lua" },
+    },
+    ["nvim-lsp-installer"] = {
+      ensure_installed = { "sumneko_lua" },
+    },
+    packer = {
+      compile_path = vim.fn.stdpath "data" .. "/packer_compiled.lua",
+    },
+  },
+
+  -- LuaSnip Options
+  luasnip = {
+    -- Add paths for including more VS Code style snippets in luasnip
+    vscode_snippet_paths = {},
+    -- Extend filetypes
+    filetype_extend = {
+      javascript = { "javascriptreact" },
+    },
+  },
+
+  -- Modify which-key registration
+  ["which-key"] = {
+    -- Add bindings
+    register_mappings = {
+      -- first key is the mode, n == normal mode
+      n = {
+        -- second key is the prefix, <leader> prefixes
+        ["<leader>"] = {
+          -- which-key registration table for normal mode, leader prefix
+          -- ["N"] = { "<cmd>tabnew<cr>", "New Buffer" },
+        },
+      },
+    },
+  },
+
+  -- CMP Source Priorities
+  -- modify here the priorities of default cmp sources
+  -- higher value == higher priority
+  -- The value can also be set to a boolean for disabling default sources:
+  -- false == disabled
+  -- true == 1000
+  cmp = {
+    source_priority = {
+      nvim_lsp = 1000,
+      luasnip = 750,
+      buffer = 500,
+      path = 250,
+    },
+  },
+
+  -- Extend LSP configuration
+  lsp = {
+    -- enable servers that you already have installed without lsp-installer
+    servers = {
+      -- "pyright"
+    },
+    -- easily add or disable built in mappings added during LSP attaching
+    mappings = {
+      n = {
+        -- ["<leader>lf"] = false -- disable formatting keymap
+      },
+    },
+    -- add to the server on_attach function
+    -- on_attach = function(client, bufnr)
+    -- end,
+
+    -- override the lsp installer server-registration function
+    -- server_registration = function(server, opts)
+    --   require("lspconfig")[server].setup(opts)
+    -- end,
+
+    -- Add overrides for LSP server settings, the keys are the name of the server
+    ["server-settings"] = {
+      -- example for addings schemas to yamlls
+      -- yamlls = {
+      --   settings = {
+      --     yaml = {
+      --       schemas = {
+      --         ["http://json.schemastore.org/github-workflow"] = ".github/workflows/*.{yml,yaml}",
+      --         ["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
+      --         ["http://json.schemastore.org/ansible-stable-2.9"] = "roles/tasks/*.{yml,yaml}",
+      --       },
+      --     },
+      --   },
+      -- },
+    },
+  },
+
+  -- Diagnostics configuration (for vim.diagnostics.config({}))
+  diagnostics = {
+    virtual_text = true,
+    underline = true,
+  },
+
+  mappings = {
+    -- first key is the mode
+    n = {
+      -- second key is the lefthand side of the map
+      ["<C-s>"] = { ":w!<cr>", desc = "Save File" },
+    },
+    t = {
+      -- setting a mapping to false will disable it
+      -- ["<esc>"] = false,
+    },
+  },
+
+  -- This function is run last
+  -- good place to configuring augroups/autocommands and custom filetypes
+  polish = function()
+    -- Set key binding
+    -- Set autocommands
+    vim.api.nvim_create_augroup("packer_conf", { clear = true })
+    vim.api.nvim_create_autocmd("BufWritePost", {
+      desc = "Sync packer after modifying plugins.lua",
+      group = "packer_conf",
+      pattern = "plugins.lua",
+      command = "source <afile> | PackerSync",
+    })
+
+    -- Set up custom filetypes
+    -- vim.filetype.add {
+    --   extension = {
+    --     foo = "fooscript",
+    --   },
+    --   filename = {
+    --     ["Foofile"] = "fooscript",
+    --   },
+    --   pattern = {
+    --     ["~/%.config/foo/.*"] = "fooscript",
+    --   },
+    -- }
+  end,
+}
+
+return config
--- home/neovim.nix
+++ home/neovim.nix
@@ -1,42 +1,7 @@
 { config, lib, pkgs, ... }:
 let
-  inherit (lib) getName mkIf optional;
   inherit (config.lib.file) mkOutOfStoreSymlink;
   inherit (config.home.user-info) nixConfigDirectory;
-
-  # theme: kanagawa.nvim
-  kanagawa-nvim = pkgs.vimUtils.buildVimPluginFrom2Nix {
-    pname = "kanagawa.nvim";
-    name = "kanagawa.nvim"; # https://github.com/rebelot/kanagawa.nvim
-    src = pkgs.fetchFromGitHub {
-      owner = "rebelot";
-      repo = "kanagawa.nvim";
-      rev = "63cb5cc1a80def7da4bb375adee1587866250a17";
-      sha256 = "sha256-TStFrx0u/MwSDcimvuwRRiyajkVq+rF5HWYCRsL4PCI=";
-    };
-  };
-
-  # go.nvim
-  go-nvim = pkgs.vimUtils.buildVimPluginFrom2Nix {
-    pname = "go.nvim";
-    name = "go.nvim"; # https://github.com/ray-x/go.nvim
-    src = pkgs.fetchFromGitHub {
-      owner = "ray-x";
-      repo = "go.nvim";
-      rev = "0fe0a9ee3367f7bd1e9c9ab30d5b7d5e66b83fc6";
-      sha256 = "sha256-DyByErbti+bBr0IZMQLwpGgrRJsFUeFK7buJyFjj1NE=";
-    };
-  };
-  guihua-lua = pkgs.vimUtils.buildVimPluginFrom2Nix {
-    pname = "guihua.lua";
-    name = "guihua.lua"; # https://github.com/ray-x/guihua.lua
-    src = pkgs.fetchFromGitHub {
-      owner = "ray-x";
-      repo = "guihua.lua";
-      rev = "dc31428364809b4388c007c227e3d5d39f1e4395";
-      sha256 = "sha256-Qi6aeHFrYChWbNr4sEE+6KPtTZSlmJRx2msYZK4Y/8s=";
-    };
-  };
 in
 {
   # Neovim
@@ -45,21 +10,15 @@ in
   programs.neovim.vimAlias = true;
   programs.neovim.vimdiffAlias = true;
 
-  # Minimal init.vim config to load Lua config. Nix and Home Manager don't currently support
-  # `init.lua`.
-  xdg.configFile."nvim/lua".source = mkOutOfStoreSymlink "${nixConfigDirectory}/configs/nvim/lua";
+  xdg.configFile."nvim/lua/user".source = mkOutOfStoreSymlink "${nixConfigDirectory}/config/nvim";
+  xdg.configFile.nvim = {
+    source = pkgs.fetchFromGitHub {
+      owner = "AstroNvim";
+      repo = "AstroNvim";
+      rev = "v1.7.0";
+      sha256 = "sha256-ui2GqSOcCjT7PGaxWSfL3C1ryabmX+CC7vHFSgPLL8w=";
+    };
+    recursive = true;
+  };
   programs.neovim.extraConfig = "lua require('init')";
-
-  programs.neovim.plugins = with pkgs.vimPlugins; [
-    guihua-lua
-    nvim-dap
-    nvim-dap-ui
-    nvim-dap-virtual-text
-    nvim-lspconfig
-    go-nvim
-    kanagawa-nvim
-    nvim-treesitter
-    plenary-nvim
-    telescope-nvim
-  ];
 }
