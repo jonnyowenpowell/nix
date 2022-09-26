5f2871f2f3401206aef5630dea95226ba516951d
Jonny Powell <jonnyowenpowell@gmail.com>
2022-09-26 12:19:19 +0100

    Added httpie brew

--- darwin/homebrew.nix
+++ darwin/homebrew.nix
@@ -44,6 +44,10 @@ in
   homebrew.masApps = {
   };
 
+  homebrew.brews = [
+    "httpie"
+  ];
+
   # If an app isn't available in the Mac App Store, or the version in the App Store has
   # limitiations, e.g., Transmit, install the Homebrew Cask.
   homebrew.casks = [
