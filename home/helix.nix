cab5cc2fa7f423ecf80c7b741bd3b0574c401d51
Jonny Powell <jonnyowenpowell@gmail.com>
2023-02-06 13:41:21 +0000

    added helix langauge config

--- home/helix.nix
+++ home/helix.nix
@@ -5,6 +5,7 @@ in
 {
   # Helix: https://github.com/helix-editor/helix
   xdg.configFile."helix/config.toml".source = builtins.toFile "config.toml" (builtins.readFile ../config/helix/config.toml);
+  xdg.configFile."helix/languages.toml".source = builtins.toFile "languages.toml" (builtins.readFile ../config/helix/languages.toml);
 
   xdg.configFile."helix/themes/kanagawa.toml".source = builtins.toFile "kanagawa.toml" ''
     # fujiWhite
