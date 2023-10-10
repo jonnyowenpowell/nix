7010fe194f6c342f4534b98fcaf99a768d3708d2
Jonny Powell <jonnyowenpowell@gmail.com>
2023-10-10 18:33:47 +0100

    fix tilt derivation

--- pkgs/tilt.nix
+++ pkgs/tilt.nix
@@ -1,8 +1,21 @@
 { lib
+, pkgs
 , buildGoModule
 , fetchFromGitHub
 }:
 
+let 
+  pkgSrc = fetchFromGitHub {
+    owner = "tilt-dev";
+    repo = "tilt";
+    rev = "8cddf4d852ce2bd027d3d577796be179a3f24380";
+    hash = "sha256-GkcP5cICPzbRJWN6ysueqedhED/MQaqcP9p2qXqpTII=";
+  };
+  tilt-ui = pkgs.mkYarnPackage {
+    name = "tilt-ui";
+    src = "${pkgSrc}/web";
+  };
+in
 buildGoModule rec {
   pname = "tilt";
   /* Do not use "dev" as a version. If you do, Tilt will consider itself
@@ -10,18 +23,22 @@ buildGoModule rec {
     source tree, which is not there once build completes.  */
   version = "0.33.6";
 
-  src = fetchFromGitHub {
-    owner = "tilt-dev";
-    repo = "tilt";
-    rev = "8cddf4d852ce2bd027d3d577796be179a3f24380";
-    hash = "sha256-GkcP5cICPzbRJWN6ysueqedhED/MQaqcP9p2qXqpTII=";
-  };
+  src = pkgSrc;
 
   vendorHash = null;
+
+  buildInputs = [ pkgs.yarn tilt-ui ];
 
   subPackages = [ "cmd/tilt" ];
 
   ldflags = [ "-X main.version=${version}" ];
+
+  preBuild = ''
+    cp -R ${tilt-ui}/libexec/tilt-ui/node_modules web/node_modules
+    chmod -R 777 web/node_modules
+    ${pkgs.yarn}/bin/yarn --cwd web build
+    cp -r web/build/* pkg/assets/build
+  '';
 
   meta = {
     description = "Local development tool to manage your developer instance when your team deploys to Kubernetes in production";
