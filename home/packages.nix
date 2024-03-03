687234f25b4daf5b40adf56988a73d4f9bb5aa52
Jonny Powell <jonnyowenpowell@gmail.com>
2024-03-03 09:38:11 +0000

    update inputs and use go 1.22

--- home/packages.nix
+++ home/packages.nix
@@ -17,7 +17,7 @@ in {
 
   # Go
   programs.go.enable = true;
-  programs.go.package = pkgs.pkgs-unstable.go_1_21;
+  programs.go.package = pkgs.pkgs-unstable.go_1_22;
   programs.go.goBin = "go/bin";
   programs.go.goPath = "go";
 
