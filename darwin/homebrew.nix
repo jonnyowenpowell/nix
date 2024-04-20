775b21199d0bd7509b9d011296f9fd189273fa6a
Jonny Powell <jonnyowenpowell@gmail.com>
2024-04-20 14:49:22 +0100

    install oterm

--- darwin/homebrew.nix
+++ darwin/homebrew.nix
@@ -32,6 +32,7 @@ in
   homebrew.global.lockfiles = true;
 
   homebrew.taps = [
+    "ggozad/formulas"
   ];
 
   homebrew.masApps = {
@@ -39,6 +40,7 @@ in
 
   homebrew.brews = [
     "httpie"
+    "ggozad/formulas/oterm"
   ];
 
   # If an app isn't available in the Mac App Store, or the version in the App Store has
