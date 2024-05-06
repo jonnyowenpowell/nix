b6d47334bd4b63f9bb0d7488f73e76845542035b
Jonny Powell <jonnyowenpowell@gmail.com>
2024-05-06 21:47:44 +0100

    set term xterm ssh

--- home/ssh.nix
+++ home/ssh.nix
@@ -3,6 +3,7 @@
 {
   home.file.".ssh/config".text = ''
     Host *
+      SetEnv TERM=xterm
       IdentityAgent /Users/jonny/Library/Containers/com.maxgoedjen.Secretive.SecretAgent/Data/socket.ssh
       HashKnownHosts yes
       UserKnownHostsFile /Users/jonny/.ssh/known_hosts
